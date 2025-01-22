681642acd0a1562743d2e499de1f571ce1451fca
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-01-22 23:21:30 +0100

    Fun with table and data fetching!

--- OperatorPortal/Web/Organizations/Router.fs
+++ OperatorPortal/Web/Organizations/Router.fs
@@ -1,11 +1,29 @@
 module Organizations.Router
 
+open System.Threading
 open Oxpecker
 open RenderBasedOnHtmx
 
 let renderOrganizations: EndpointHandler =
-    render Template.Partial Template.FullPage
-    
-let Endpoints = [
-    route "/" renderOrganizations
-]
+    fun ctx ->
+        let shouldFetchData =
+            match ctx.TryGetQueryValue "data" with
+            | Some str when str = "true" -> true
+            | _ -> false
+
+        if shouldFetchData then
+            Thread.Sleep(1500)
+
+            ctx.WriteHtmlView(
+                Template.DataTemplate
+                    [ { Name = "Test Org"
+                        City = "Pruszków"
+                        ContactPerson = "Kazik Barazik" }
+                      { Name = "OpenAI"
+                        City = "Piaseczno"
+                        ContactPerson = "Sam Altman" } ]
+            )
+        else
+            ctx |> render Template.Partial Template.FullPage
+
+let Endpoints = [ route "/" renderOrganizations ]
--- OperatorPortal/Web/Organizations/Template.fs
+++ OperatorPortal/Web/Organizations/Template.fs
@@ -2,16 +2,43 @@ module Organizations.Template
 
 open Layout.Navigation
 open Oxpecker.ViewEngine
+open Oxpecker.Htmx
 open Layout
 
+type OrganizationDto =
+    { Name: string
+      ContactPerson: string
+      City: string }
+
 let private page =
-    div () { "Organizations Page" }
-    
+    div (hxGet = "/organizations?data=true", hxTrigger = "load") { "Loading..." }
+
+let DataTemplate (data: OrganizationDto list) =
+    div () {
+        table (class' = "striped") {
+            thead () {
+                tr () {
+                    th () { "Name" }
+                    th () { "Contact person" }
+                    th () { "City" }
+                }
+            }
+
+            tbody () {
+                for row in data do
+                    tr () {
+                        td () { row.Name }
+                        td () { row.ContactPerson }
+                        td () { row.City }
+                    }
+            }
+        }
+    }
+
 let Partial =
     Fragment() {
         Body.Template page Page.Organizations
         Head.ReplaceTitle <| Page.Organizations.ToTitle()
     }
 
-let FullPage =
-    Head.Template Partial (Page.Organizations.ToTitle())
+let FullPage = Head.Template Partial (Page.Organizations.ToTitle())
