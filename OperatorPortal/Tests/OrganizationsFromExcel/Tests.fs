ca3b452d196fc5e399758a6581353dd64eef7f09
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-02-03 16:43:43 +0100

    Add first test to get the data, add fsharp utility for dapper for easier db operations, add migrations

--- OperatorPortal/Tests/OrganizationsFromExcel/Tests.fs
+++ OperatorPortal/Tests/OrganizationsFromExcel/Tests.fs
@@ -1,16 +1,28 @@
 module Tests.OrganizationsFromExcel.Tests
 
+open PostgresPersistence
 open Xunit
 open FsUnit.Xunit
 open Organizations.Database.csvLoader
-    
+open DapperFsharp
+open Organizations.Database.OrganizationRow
+
 [<Literal>]
-let ResolutionFolder = __SOURCE_DIRECTORY__      
-let sampleOrgs =
-    Orgs.GetSample()
-    
+let ResolutionFolder = __SOURCE_DIRECTORY__
+
+let sampleOrgs = Orgs.GetSample()
+
 [<Fact>]
-let ``Sample excel can be parsed to organizations and saved`` ()=
-    let row = sampleOrgs.Rows |> Seq.head
-    let a = parse row
-    true |> should equal false
+let ``Sample excel can be parsed to organizations and saved`` () =
+    async {
+        let row = sampleOrgs.Rows |> Seq.head
+        let a = parse row
+        
+        let con = dbConnect("Host=localhost;Port=5432;User Id=postgres;Password=Strong!Passw0rd;Database=food_bank;")
+        
+        let! db = con()
+        
+        let! rows = db |> dbQuery<OrganizationRow>("SELECT * FROM organizacje")
+        
+        true |> should equal false
+    }
---
+++ OperatorPortal/migrations.fsx
@@ -0,0 +1,33 @@
+#r "nuget: DbUp"
+#r "nuget: dbup-postgresql"
+
+open System
+open System.IO
+open DbUp
+
+let logAndParseEngineResult (result: Engine.DatabaseUpgradeResult) =
+    match result.Successful with
+     | true ->
+         Console.ForegroundColor <- ConsoleColor.Green
+         Console.WriteLine "Success"
+         Console.ResetColor()
+         0
+     | false ->
+         Console.ForegroundColor <- ConsoleColor.Red
+         Console.WriteLine result.Error
+         Console.ResetColor()
+         -1
+         
+let connectionString =
+  match fsi.CommandLineArgs |> Array.tryItem 1 with
+  | Some connectionString -> connectionString
+  | None ->
+      "Host=localhost;Port=5432;User Id=postgres;Password=Strong!Passw0rd;Database=food_bank;"
+
+Console.WriteLine(connectionString)
+let path = Path.Combine(Directory.GetCurrentDirectory(), "Web", "Organizations", "Database")
+
+DeployChanges.To.PostgresqlDatabase(connectionString)
+             .WithScriptsFromFileSystem(path, fun sqlFilePath -> true)
+             .LogToConsole().Build().PerformUpgrade()
+|> logAndParseEngineResult
