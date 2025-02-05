f79ecc5dec6a05cf31f6d3224a5165a6c0eb84b0
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-02-05 14:26:43 +0100

    Fighting with CI #17

--- OperatorPortal/Tests/Tools/Setup.fs
+++ OperatorPortal/Tests/Tools/Setup.fs
@@ -1,37 +1,11 @@
 module Tests.Setup
 
 open System
-open System.Collections.Concurrent
 open System.Diagnostics
 open System.IO
 open System.Threading
-open System.Threading.Tasks
 open Testcontainers.PostgreSql
 open Xunit
-
-let run scriptPath  =
-    let cts = TaskCompletionSource()
-    use cmd = new Process()
-    let cmdInfo = ProcessStartInfo()
-    cmdInfo.FileName <- "bash"
-    cmdInfo.RedirectStandardInput <- true
-    cmdInfo.RedirectStandardOutput <- true
-    cmdInfo.RedirectStandardError <- true
-    cmdInfo.UseShellExecute <- false
-    cmdInfo.WorkingDirectory <- scriptPath
-    cmd.StartInfo <- cmdInfo
-    let output = new BlockingCollection<string>()
-    let append = fun (args: DataReceivedEventArgs) ->
-        output.Add args.Data
-        if args.Data = "_finito_" then cts.SetResult()
-    cmd.ErrorDataReceived.Add append
-    cmd.OutputDataReceived.Add append
-    cmd.Start() |> ignore
-    cmd.BeginOutputReadLine();
-    cmd.BeginErrorReadLine();
-    cmd.StandardInput.WriteLine($"dotnet fsi migrations.fsx ; echo _finito_")
-    cts.Task.Wait()
-    output |> Seq.toList |> List.filter(fun output -> output <> "_finito_")
 
 let runFSharpScript scriptPath =
     let psi = ProcessStartInfo("dotnet", $"fsi {scriptPath}")
@@ -40,9 +14,9 @@ let runFSharpScript scriptPath =
     psi.UseShellExecute <- false
     File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "migrations_pre.txt"), "WILL RUN MIGRATIONS")
     use proc = Process.Start(psi)
+    proc.WaitForExit()
     let output = proc.StandardOutput.ReadToEnd()
     let errors = proc.StandardError.ReadToEnd()
-    proc.WaitForExit()
     printfn "Script Output: %s" output
     File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "migrations_out.txt"), output)
     if not (String.IsNullOrWhiteSpace(errors)) then
@@ -61,8 +35,7 @@ type Setup() =
                             .Build();
         container.StartAsync() |> Async.AwaitTask |> Async.RunSynchronously
         Thread.Sleep(1000)
-        run "/home/marcin/code/food-bank/OperatorPortal"
-        ()
+        runFSharpScript "/home/marcin/code/food-bank/OperatorPortal/migrations.fsx"
     
     interface IDisposable with
         member _.Dispose() = ()
