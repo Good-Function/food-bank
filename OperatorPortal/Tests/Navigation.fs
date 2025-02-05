02999b50359a76b9cf0075870678803e37859d6c
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-02-05 18:19:20 +0100

    Checking test on CI #5

--- OperatorPortal/Tests/Navigation.fs
+++ OperatorPortal/Tests/Navigation.fs
@@ -16,16 +16,12 @@ let ``Navigations contains /ogranizations, /applications, /team links`` () =
         api.DefaultRequestHeaders.Add(Authentication.FakeAuthenticationHeader, "TestUser")
         // Act
         let! response = api.GetAsync "/organizations"
-        let! stuff = response.Content.ReadAsStringAsync()
-        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "test.txt"), stuff)
-        response.StatusCode |> should equal 100
-        stuff |> should equal "teradascie"
         // Assert
+        response.StatusCode |> should equal HttpStatusCode.OK
         let! document = response.HtmlContent()
         
         let navLinks = document.CssSelect "nav ul li a"
                        |> Seq.map(_.Attribute("href").Value())
                        |> Seq.toList
         navLinks |> should equal [ "/organizations"; "/applications"; "/team" ]    
-        response.StatusCode |> should equal HttpStatusCode.OK
     }
