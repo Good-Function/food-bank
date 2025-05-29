e370f6c48a4d3d289acfb271882918d4b3b7c27b
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-05-30 00:02:17 +0200

    fix blob storage min length

--- OperatorPortal/Web/Organizations/BlobStorage.fs
+++ OperatorPortal/Web/Organizations/BlobStorage.fs
@@ -9,7 +9,7 @@ open Organizations.Application.DocumentHandlers
 let upload (serviceClient: BlobServiceClient): UploadBlob =
     fun (teczkaId, uploadDocument) ->
         async {
-            let containerClient = serviceClient.GetBlobContainerClient $"{teczkaId}"
+            let containerClient = serviceClient.GetBlobContainerClient $"teczka-{teczkaId}"
             let! _ = containerClient.CreateIfNotExistsAsync() |> Async.AwaitTask
             let blobClient = containerClient.GetBlobClient uploadDocument.FileName
             let! _ = blobClient.UploadAsync(uploadDocument.ContentStream, true) |> Async.AwaitTask
@@ -19,17 +19,17 @@ let upload (serviceClient: BlobServiceClient): UploadBlob =
 let delete (serviceClient: BlobServiceClient) : DeleteFile =
     fun (teczkaId: Commands.TeczkaId, fileName: FileName) -> 
         async {
-             let containerClient = serviceClient.GetBlobContainerClient($"{teczkaId}")
+             let containerClient = serviceClient.GetBlobContainerClient($"teczka-{teczkaId}")
              let! _ = containerClient.DeleteBlobIfExistsAsync(fileName) |> Async.AwaitTask
              return ()
         }
     
 let generateDownloadUri (serviceClient: BlobServiceClient) : GenerateDownloadUri =
     fun (teczkaId: Commands.TeczkaId, fileName:string) ->
-        let containerClient = serviceClient.GetBlobContainerClient($"{teczkaId}")
+        let containerClient = serviceClient.GetBlobContainerClient($"teczka-{teczkaId}")
         let blobClient = containerClient.GetBlobClient(fileName)
         let sasBuilder = BlobSasBuilder(
-            BlobContainerName = $"{teczkaId}",
+            BlobContainerName = $"teczka-{teczkaId}",
             BlobName = fileName,
             Resource = "b",
             ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(3.0)
