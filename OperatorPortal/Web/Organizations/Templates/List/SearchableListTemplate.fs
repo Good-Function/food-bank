5234598671fb8b50ea35ed79206a73e384e430e3
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-06-19 20:44:04 +0200

    Extract table header rendering logic to a separate `TableHeaderBuilder` module for improved reusability and cleaner templates.

--- OperatorPortal/Web/Organizations/Templates/List/SearchableListTemplate.fs
+++ OperatorPortal/Web/Organizations/Templates/List/SearchableListTemplate.fs
@@ -2,15 +2,12 @@ module Organizations.List.SearchableListTemplate
 
 open Layout
 open Layout.Navigation
-open Organizations.Application.ReadModels.QueriedColumn
 open Organizations.Application.ReadModels.OrganizationSummary
 open Organizations.Templates.List
 open Oxpecker.ViewEngine
 open Oxpecker.Htmx
 open Web.Organizations
 open PageComposer
-open Organizations.Templates.List.Filterable
-open Organizations.Templates.List.Sortable
 
 let createFilterStateHolder filter =
             match filter.SortBy with
@@ -46,98 +43,7 @@ let Template (query: Query) =
             div (style = "overflow-x: scroll; height: 70vh;";) {
                 table (class' = "striped big-table") {
                     thead () {
-                        tr (id="OrganizationHeadersRow") {
-                            th (style = "width:85px;") {
-                                sortable {
-                                    Column = Teczka
-                                    CurrentSortBy = query.SortBy
-                                }
-                            }
-                            th (style = "width:290px;") {
-                                sortable {
-                                    Column = NazwaPlacowkiTrafiaZywnosc
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = NazwaPlacowkiTrafiaZywnosc
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:300px;") {
-                                sortable {
-                                    Column = AdresPlacowkiTrafiaZywnosc
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = AdresPlacowkiTrafiaZywnosc
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:220px;") {
-                                sortable {
-                                    Column = GminaDzielnica
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = GminaDzielnica
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:200px;") {
-                                sortable {
-                                    Column = FormaPrawna
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = FormaPrawna
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:200px;") {
-                                sortable {
-                                    Column = Kategoria
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = Kategoria
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:200px;") {
-                                sortable {
-                                    Column = Beneficjenci
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.StringFilter
-                                    Column = Beneficjenci
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:155px;") {
-                                sortable {
-                                    Column = LiczbaBeneficjentow
-                                    CurrentSortBy = query.SortBy
-                                }
-                                filterable {
-                                    Type = FilterType.NumberFilter
-                                    Column = LiczbaBeneficjentow
-                                    CurrentFilters = query.Filters
-                                }
-                            }
-                            th (style = "width:150px;") {
-                                sortable {
-                                    Column = OstatnieOdwiedzinyData
-                                    CurrentSortBy = query.SortBy
-                                }
-                            }
-                            th (style = "width:110px;") { "Kontakt" }
-                        }
+                        TableHeaderBuilder.build query
                     }
                     Indicators.TableShimmeringRows 6 9
                     tbody (id = "OrganizationsList") { }
