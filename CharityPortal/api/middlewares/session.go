51e15ab4e12cd14e966b256ed5ba0299c1801c83
Kamil Zielinski <k.zielinski@text.com>
2025-07-16 22:00:57 +0200

    Test user without auth on dev

--- CharityPortal/api/middlewares/session.go
+++ CharityPortal/api/middlewares/session.go
@@ -6,17 +6,30 @@ import (
 )
 
 type SessionMiddleware struct {
-	authProvider auth.AuthProvider
+	authProvider   auth.AuthProvider
+	appEnvironment string
 }
 
-func NewSessionMiddleware(authProvicer auth.AuthProvider) *SessionMiddleware {
+func NewSessionMiddleware(authProvicer auth.AuthProvider, appEnvironment string) *SessionMiddleware {
 	return &SessionMiddleware{
-		authProvider: authProvicer,
+		authProvider:   authProvicer,
+		appEnvironment: appEnvironment,
 	}
 }
 
 func (am *SessionMiddleware) Session(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if am.appEnvironment == "development" {
+			// In development mode, we skip session management for simplicity.
+			sessionData := auth.UserClaims{
+				Name:  "test",
+				Email: "test@test.com",
+				Sub:   "test-sub",
+			}
+			r = r.WithContext(auth.SetUserContext(r.Context(), &sessionData))
+			h.ServeHTTP(w, r)
+			return
+		}
 		cookie, err := r.Cookie("session")
 		if err != nil {
 			h.ServeHTTP(w, r)
