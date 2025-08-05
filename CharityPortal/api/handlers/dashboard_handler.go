2505d9cdb6bc0fc116191e63076e6f194b869cd0
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-08-05 09:34:13 +0200

    dashboard change for user

--- CharityPortal/api/handlers/dashboard_handler.go
+++ CharityPortal/api/handlers/dashboard_handler.go
@@ -13,12 +13,6 @@ func NewDashboardHandler() *DashboardHandler {
 }
 
 func (ph *DashboardHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user := r.Context().Value(auth.UserContextKey{})
-    if user == nil {
-        http.Error(w, "User not in context", http.StatusUnauthorized)
-        return
-    }
-
-    email := user.(string)
+	email := r.Context().Value(auth.UserContextKey{}).(string)
 	views.Base(views.Dashboard(), email).Render(r.Context(), w)
 }
