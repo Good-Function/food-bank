be76a9d96c191baef7a17e2d214e2f6bce8bd2db
Marcin 🐈 Golenia <marcin.golenia@ignite.no>
2025-09-12 17:54:09 +0200

    fix test

--- OperatorPortal/Tests/Organizations/OrganizationsDataApi.fs
+++ OperatorPortal/Tests/Organizations/OrganizationsDataApi.fs
@@ -1,9 +1,11 @@
 module OrganizationsDataApi
 
+open System
 open System.Net
 open System.Text.Json
 open Organizations.Domain.Organization
 open Tests
+open Tests.Arranger
 open Xunit
 open Tools.TestServer
 open FsUnit.Xunit
@@ -13,7 +15,7 @@ open Organizations.Database.OrganizationsDao
 let ``/ogranizations/kontakty?email returns kontakty by email`` () =
     task {
         // Arrange
-        let org =  Arranger.AnOrganization()
+        let org = AnOrganization() |> setEmail (Guid.NewGuid().ToString())
         do! org |> (save Tools.DbConnection.connectDb)
         let api = runTestApi()
         // Act
